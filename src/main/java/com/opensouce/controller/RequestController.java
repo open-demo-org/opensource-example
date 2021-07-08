07c14c683aef14c5b630ae21352e924d70dc6c74
xuup <xupeng0727@foxmail.com>
2021-07-08 14:17:23 +0800

    init commit

---
+++ src/main/java/com/opensouce/controller/RequestController.java
@@ -0,0 +1,15 @@
+package com.opensouce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/request")
+public class RequestController {
+
+    @GetMapping("/send")
+    public String request(){
+        return "this is test result";
+    }
+}
